788578ed9584c4836c7556da8c16dcf6a5f13152
FutureScript <fernandobaxter1@gmail.com>
2018-11-28 01:09:40 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -69,12 +69,19 @@ end
 
 def selective_tweet_shortener(tweet)
   if tweet.length > 140
-    word_substituter(tweet)
+    # word_substituter(tweet)
   else
     tweet
   end
 end
 
+def shortened_tweet_truncator(tweet)
+  if tweet.length > 140
+
+    then 
+    truncate after 140 to [...]
+end
+end
 #   tweet.split.map do |phrase|
 #     if tweet.length > 140
 #       word_substituter(phrase)
